eafd8c114b83be089ceb35d5e3cd3d4b1277190a
Todd Stellanova <tstellanova@gmail.com>
2018-02-24 19:50:44 +0000

    Additional support for read_write ioctl

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # rpi3_firmware_access
-Allow access to Raspberry Pi 3 (RPi3) firmware properties from Rust
+Allow access to Raspberry Pi 3 (RPi3) firmware properties from Rust.
+This allows us to read and write firmware properties of the VideoCore IV GPU,
+which control a broad range of RPi3 behaviors.
+
+For example, you can read and write GPIOs controlled by the VideoCore 
+(which are completely separate from CPU GPIOs that can be accessed by `pigpiod`, for example).
 
 See [RPi Mailbox property interface](https://github.com/raspberrypi/firmware/wiki/Mailbox-property-interface) for more info on accessing firmware properties by tag.  
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,56 @@
+/*
+Copyright (c) 2018 Todd Stellanova
+
+LICENSE: See LICENSE file
+*/
+#![crate_type = "lib"]
+
+
+#[macro_use]
+extern crate nix;
+
 mod bindings;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use std::fs::File;
+use std::os::unix::io::{ AsRawFd };
+
+use bindings::*;
+
+const FW_DEVICE_PATH: &'static str = "/dev/vcio";
+
+  // want _IOWR(100, 0, char*)
+  const VC_FW_IOC_MAGIC: u8 = 100 ;
+  const VC_FW_IOC_IF_NUM: u8 = 0;
+
+
+  ioctl!(readwrite_buf readwrite_vc_fw with VC_FW_IOC_MAGIC, VC_FW_IOC_IF_NUM; u32 );
+
+
+fn touch_fw_property(tag: RPiFwPropTag, prop_data: &mut [u32] ) {
+  let f = File::open(FW_DEVICE_PATH)
+    .expect("Could not open FW_DEVICE_PATH");
+  let transaction_buf_len = prop_data.len() + 6 ;
+  let prop_data_size = 4 * prop_data.len();
+  let mut req= Vec::with_capacity(transaction_buf_len);
+  req[0] = RPI_FWPROP_STATUS_REQUEST;
+  req[1] = tag as u32;
+  req[2] = prop_data_size as u32;
+  //req[3] = 0; //response slot
+  //TODO copy prop_data into req
+ 
+  unsafe {
+    let rc = readwrite_vc_fw(f.as_raw_fd(), &mut req);
+  }
 }
+
+fn read_gpio_value(gpio: u32) -> u32 {
+  let mut buf: [u32; 2] = [0; 2];
+  buf[0] = gpio;
+  touch_fw_property(RPiFwPropTag::GetGpioState, &mut buf);
+  buf[1]
+}
+
+pub fn get_power_status() -> u32 {
+  read_gpio_value(135)
+}
+
