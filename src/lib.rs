61a481b8db7f2246da93ac81806946662537ac34
Todd Stellanova <tstellanova@gmail.com>
2018-02-28 16:35:30 +0000

    Fix basic tests

--- src/lib.rs
+++ src/lib.rs
@@ -19,12 +19,18 @@ mod tests {
   #[test]
   fn check_soc_get_temperature() {
    let temp = get_soc_temperature(0);
-   assert_eq!(temp, 1); 
+   assert!(temp > 1000);
+   //convert to Celsius
+   let temp = (temp as f64) / 1000.0;
+   // temp should be in a reasonable Celsius range
+   assert!(temp > 0.0);
+   assert!(temp < 90.0);
   }
 
   #[test]
   fn check_power_status() {
     let status = get_power_status();
+    //we should have adequate power
     assert_eq!(status, 1);
    }
 }
